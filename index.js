e6e390e3a3a8841c1f6006e31e67518dab2e7f84
a5medHub <ayassine.auce@gmail.com>
2022-11-22 18:17:56 +0200

    update arr

--- index.js
+++ index.js
@@ -52,8 +52,17 @@ const characters = [
   "y",
   "z",
 ];
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const symbols = [
+const symbolsNumbers = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
   "~",
   "`",
   "!",
